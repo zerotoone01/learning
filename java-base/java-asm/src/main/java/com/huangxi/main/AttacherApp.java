3e9708ba2874d5736e15f1eaa8cc3039643345e1
huangluojun <huang.luojun@intellif.com>
2020-07-14 17:33:54 +0800

    java agent测试

---
+++ java-base/java-asm/src/main/java/com/huangxi/main/AttacherApp.java
@@ -0,0 +1,24 @@
+package com.huangxi.main;
+
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+
+import java.io.IOException;
+
+/**
+ * @description 利用Attach API，将我们打包好的agent jar包Attach到指定的JVM pid上
+ *              com.huangxi.agent.TestAgent
+ * @date 2020-07-14
+ */
+public class AttacherApp {
+    public static void main(String[] args) throws AttachNotSupportedException, IOException, AgentLoadException, AgentInitializationException {
+
+        System.out.println(System.getProperty("user.dir"));
+        // 传入目标 JVM pid  com.huangxi.bytecode.BaseJavassist
+        VirtualMachine vm = VirtualMachine.attach("3004");
+        // maven构建好的jar拷贝到resource文件夹下
+        vm.loadAgent("./java-asm/src/main/resources/java-asm-1.0-SNAPSHOT.jar");
+    }
+}
---
+++ java-base/java-asm/src/test/java/agent/AgentTest.java
@@ -0,0 +1,13 @@
+package agent;
+
+/**
+ * @author huang.luo.jun
+ * @description TODO
+ * @date 2020-07-14
+ */
+public class AgentTest {
+
+    public static void main(String[] args) {
+        System.out.println(" agent test!");
+    }
+}
