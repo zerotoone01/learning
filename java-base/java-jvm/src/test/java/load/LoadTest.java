f4868cf07f37d1c29590e70dbdac4dd8e51a62e9
huangluojun <huang.luojun@intellif.com>
2020-07-15 15:25:32 +0800

    jvm load

---
+++ java-base/java-jvm/src/test/java/load/LoadTest.java
@@ -0,0 +1,24 @@
+package load;
+
+
+import com.huangxi.load.SingleTon;
+import org.junit.Test;
+
+/**
+ * @description TODO
+ * @date 2020-07-15
+ */
+public class LoadTest {
+    @Test
+    public void loadTest() {
+//        SingleTon singleTon = SingleTon.getInstance();
+//        System.out.println("count1=" + singleTon.count1);
+//        System.out.println("count2=" + singleTon.count2);
+
+        System.out.println("count1=" + SingleTon.count1);
+        System.out.println("count2=" + SingleTon.count2);
+        //相当于两次
+        // result>>>>>>>> count1=1 count2=0
+    }
+
+}
